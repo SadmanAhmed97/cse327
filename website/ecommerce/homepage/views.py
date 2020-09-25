be8e60a0a3d3c89154fa4960d9d04360dd0cd9bf
SadmanAhmed97 <51294650+SadmanAhmed97@users.noreply.github.com>
2020-09-25 07:37:55 +0600

    cart funtions are added
    a demo user is created and all the views and methods of how to display cart fucntions are completed.

--- website/ecommerce/homepage/views.py
+++ website/ecommerce/homepage/views.py
@@ -1,18 +1,36 @@
 from django.shortcuts import render
+from .models import *
 
 
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products': products}
     return render(request, 'homepage/index.html', context)
 
 
 def cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {'items': items, 'order': order}
     return render(request, 'homepage/cart.html', context)
 
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        items = []
+
+    context = {'items': items, 'order': order}
     return render(request, 'homepage/checkout.html', context)
 
 
